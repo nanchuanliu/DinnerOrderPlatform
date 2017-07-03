33bda5a6eb1b1e94e546609fb9d2cd9c1e2b46af
nanchuanliu <349249441@qq.com>
2017-07-03 18:34:17 +0800

    DinnerOrderApp

---
+++ DinnerOrderApp/app/src/main/java/com/lzw/order/dinnerorderapp/services/DataService.java
@@ -0,0 +1,13 @@
+package com.lzw.order.dinnerorderapp.services;
+
+import android.telecom.Call;
+
+import rx.Observable;
+
+/**
+ * Created by LZW on 2017/07/03.
+ */
+public interface DataService {
+
+
+}
