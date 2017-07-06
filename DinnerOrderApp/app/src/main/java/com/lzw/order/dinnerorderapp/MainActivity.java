d8cb6b3102e358fd38311465d03cdbb6a67d1a0a
nanchuanliu <349249441@qq.com>
2017-07-06 18:04:09 +0800

    DinnerOrderApp

--- DinnerOrderApp/app/src/main/java/com/lzw/order/dinnerorderapp/MainActivity.java
+++ DinnerOrderApp/app/src/main/java/com/lzw/order/dinnerorderapp/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.lzw.order.dinnerorderapp.fragment.HomeFragment;
 
@@ -15,10 +19,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (getSupportActionBar() != null){
+        setContentView(R.layout.activity_main3);
+/*        if (getSupportActionBar() != null){
             getSupportActionBar().hide();
-        }
+        }*/
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         checkFirstStart();
 
